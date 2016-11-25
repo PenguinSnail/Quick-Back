0ec01dc78ad3c6aac923ae9552cb30db9e5b1082
PenguinSnail <penguinsnail@onebuttonmouse.com>
2016-11-25 17:26:28 -0500

    test script for btrfs subvolume detection

--- subvolumes.sh
+++ subvolumes.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-sudo btrfs subvolume list -t / | tail -n +3 | awk '{ print $4 }'
+SRC=/var
+
+sudo btrfs subvolume list -t / | tail -n +3 | awk '{ print $4 }' > /tmp/quick-back-subvolumes
+while read path ; do
+	if [ -d "$SRC"/"$path" ]; then
+		echo "/$path subvolume exists in source"
+	fi
+done < /tmp/quick-back-subvolumes
 
 exit
