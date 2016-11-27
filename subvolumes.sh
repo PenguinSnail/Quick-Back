5a400540fd4d50e84726595e0c89366d597e00e1
PenguinSnail <penguinsnail@onebuttonmouse.com>
2016-11-27 08:10:54 -0500

    properly detect nested subvolumes

--- subvolumes.sh
+++ subvolumes.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
+#!/bin/dash
 
-SRC=/var
-
+SRC=/
+echo $SRC
 sudo btrfs subvolume list -t / | tail -n +3 | awk '{ print $4 }' > /tmp/quick-back-subvolumes
 while read path ; do
-	if [ -d "$SRC"/"$path" ]; then
-		echo "/$path subvolume exists in source"
-	fi
+path=/$path
+case $path in
+	$SRC* )
+		echo "$path exists"
+	;;
+esac
 done < /tmp/quick-back-subvolumes
 
 exit
